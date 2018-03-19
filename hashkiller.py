5004705ad1adcfcf445cdda1e94b4b4643193a93
miguelagr <miguelagr23@gmail.com>
2018-03-19 01:25:19 -0500

    act

--- hashkiller.py
+++ hashkiller.py
@@ -3,17 +3,13 @@ import time
 import psycopg2
 
 print time.clock()
+
 conn = psycopg2.connect("dbname=root user=root password=hola123.,")
-
-
 cur = conn.cursor()
-
 
 for i in hashlib.algorithms_guaranteed:
     cur.execute("CREATE TABLE %s (id serial PRIMARY KEY, plain varchar, hash varchar);" % i)
     conn.commit()
-
-
 
 with open("rockyou.txt",'r') as f:
     for i in f.readlines():
@@ -27,12 +23,5 @@ with open("rockyou.txt",'r') as f:
 
 cur.close()
 conn.close()
-
-
-for i in hashlib.algorithms_available:
-    print i
-    m = hashlib.new(i)
-    m.update("hola")
-    print m.hexdigest()
 
 print time.clock()
