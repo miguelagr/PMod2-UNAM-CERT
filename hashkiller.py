081b18d4f963a82d87bd7a1ebdad239deae7c003
miguelagr <miguelagr23@gmail.com>
2018-03-21 21:25:08 -0500

    Mejoras

--- hashkiller.py
+++ hashkiller.py
@@ -7,66 +7,93 @@ import time
 import psycopg2
 import sys
 
-def genera_bd(fname):
-	conn = psycopg2.connect("dbname=root user=root password=hola123.,")
-	cur = conn.cursor()
-	for i in hashlib.algorithms_guaranteed:
-            cmd = "select (tablename) from pg_tables where schemaname like 'public' and tablename like '%s'" % (i)
-            cur.execute(cmd)
-            if cur.fetchone() is None:
-	        cur.execute("CREATE TABLE %s (id serial PRIMARY KEY, plain varchar, hash varchar);" % i)
-	        conn.commit()
+def genera_bd(fname,tabla):
+    """
+    Genera la base de datos con todos los hashes de cada cadena de un archivo de entrada
+    Argumento:
+        Nombre del archivo donde se sacan las cadenas en claro (str)
+        Nombre de la tabla (str)
+    Salida:
+        None
+    """
+    conn = psycopg2.connect("dbname=root user=root password=hola123.,")
+    cur = conn.cursor()
+    cmd = "select (LOWER(tablename)) from pg_tables where schemaname like 'public' and tablename like LOWER('%s')" % (tabla)
+    cur.execute(cmd)
+    if cur.fetchone() is None:
+        cur.execute("CREATE TABLE %s (id serial PRIMARY KEY, plain varchar%s);" % (tabla, ', "%s" varchar' * len(hashlib.algorithms_available) % tuple(hashlib.algorithms_available)))
+	conn.commit()
 	with open(fname,'r') as f:
 	    for i in f.readlines():
-	        for j in hashlib.algorithms_guaranteed:
-	            m = hashlib.new("%s" % j)
-	            m.update(i)
-	            cmd = "INSERT INTO %s(plain, hash) VALUES ('%s','%s');" % (j,i[:-1],m.hexdigest())
-	            cur.execute(cmd)
-	            conn.commit()
+                    p = i
+	       	    m = [hashlib.new("%s" % j) for j in hashlib.algorithms_available]
+	            for mi in m:
+                        mi.update(p)
+                    mh = tuple([i.hexdigest() for i in m])
+                    cmd = "INSERT INTO %s(plain, %s) VALUES ('%s', %s);" % (tabla, ('"%s",' * len(hashlib.algorithms_available) % tuple(hashlib.algorithms_available))[:-1], p, ("'%s'," * len(mh) % tuple(mh))[:-1])
+                    cur.execute(cmd)
+	conn.commit()
 	cur.close()
 	conn.close()
 
-def busca_hash(digest,algo):
-	plain = []
-        conn = psycopg2.connect("dbname=root user=root password=hola123.,")
-	cur = conn.cursor()
-        for i in algo:
-            cmd = "select (tablename) from pg_tables where schemaname like 'public' and tablename like '%s'" % (i)
-            cur.execute(cmd)
-            if cur.fetchone():
-	        cmd = "SELECT plain FROM %s WHERE hash like '%s'" % (i,digest)
+def busca_hash(tabla,digest,algo):
+    """
+    Busca el texto en claro que corresponde al hash de la entrada
+    Argumentos:
+        Tabla de busqueda (str)
+        Cadena del digest en formato hexadecimal (str)
+        Algoritmos posibles para el digest (str[])
+    Salida:
+        Texto en claro correspondiente al hash de entrada (str[])
+    """
+    plain = []
+    conn = psycopg2.connect("dbname=root user=root password=hola123.,")
+    cur = conn.cursor()
+    for i in algo:
+        if i in hashlib.algorithms_available:
+                cmd = "select (tablename) from pg_tables where schemaname like 'public' and tablename like '%s'" % (tabla)
 	        cur.execute(cmd)
-                e = cur.fetchone()
-                if e:
-                    plain.append(e[0])
-	cur.close()
-	conn.close()
-        return plain
+	        if cur.fetchone():
+	            cmd = "SELECT plain FROM %s WHERE %s like '%s'" % (tabla,i,digest)
+                    cur.execute(cmd)
+	            e = cur.fetchone()
+	            if e:
+	                plain.append(e[0])
+    cur.close()
+    conn.close()
+    return plain
 
 def identifica(hashh):
-	tamano = len(hashh) * 4
-	if tamano == 128:
-	    return ['md5','md4','NTLM']
-	elif tamano == 160:
-	    return ['sha1']
-	elif tamano == 224:
-	    return ['SHA224']
-	elif tamano == 256:
-	    return ['SHA256']
-	elif tamano == 384:
-	    return ['SHA384']
-	elif tamano == 512:
-	    return ['SHA512']
-	else:
-            sys.exit("hash invalido")
+    """
+    Funcion que identifica el tipo de hash de acuerdo con su logitud
+    Argumentos:
+        El hash en formato hexadecimal (str)
+    Salida:
+        Lista de los posibles tipos de hashes de la cadena de entrada (str[])
+    """
+    tamano = len(hashh) * 4
+    if tamano == 128:
+	return ['md5','md4','NTLM']
+    elif tamano == 160:
+	return ['sha1']
+    elif tamano == 224:
+	return ['SHA224']
+    elif tamano == 256:
+	return ['SHA256']
+    elif tamano == 384:
+	return ['SHA384']
+    elif tamano == 512:
+	return ['SHA512']
+    else:
+        sys.exit("hash invalido")
 
-#genera_bd("rockyou.txt")
+#print genera_bd("rockyou.txt","algos")
 
 digest = 'd577273ff885c3f84dadb8578bb41399'
-busca_hash(digest,identifica(digest))
-print busca_hash(sys.argv[1],identifica(sys.argv[1]))
+for i in busca_hash("algos",digest,identifica(digest)):
+    print i
+#print busca_hash("rockyou",sys.argv[1],identifica(sys.argv[1]))
 digest = '2672275fe0c456fb671e4f417fb2f9892c7573ba'
-for i in busca_hash(digest,identifica(digest)):
+for i in busca_hash("rockyou",digest,identifica(digest)):
     print i
 
--- shadow.py
+++ shadow.py
@@ -7,9 +7,16 @@ import time
 import crypt
 
 
-def obtener_salt():
+def obtener_salt(shadowf):
+    """
+    Obtiene todos los salts presentes en un archivo con el formato del archivo shadow
+    Argumentos:
+        Nombre del archivo con formato shadow (str)
+    Salida:
+        Tupla con los salts presentes y su respectivo hash (str)
+    """
     salts = []
-    with open("/etc/shadow",'r') as f:
+    with open(shadowf,'r') as f:
         l = f.readline()
         while l:
             s = l.split(':')
@@ -18,14 +25,26 @@ def obtener_salt():
             l = f.readline()
     return salts
 
-def obtener_pass(arch):
+def obtener_pass(arch,sh):
+    """
+    Obtiene en tiempo real todos los hashes resultado de la combinacion de una lista de salts con cada una
+    de las contraseñas presentes en un archivo de contraseñas imprimiendo todas las contraseñas en claro 
+    que generan coincidencias
+    Argumentos:
+        Nombre del archivo con todas las contraseñas en claro (str)
+        Tupla de salts con su respectivo hash ((str,str)[])
+    Salida:
+        Lista de coincidencias encontradas (str[])
+    """
     with open(arch) as f:
+        t = []
         l = f.readline()
         while l:
-            for i in obtener_salt():
+            for i in sh:
                 c = crypt.crypt(l[:-1],i[0])
                 if i[1] == c:
-                    print l     
+                    t.append((l,i[1]))     
             l = f.readline()
+        return t
 
-obtener_pass("rockyou.txt")
+print obtener_pass("rockyou.txt",obtener_salt("shadow"))
